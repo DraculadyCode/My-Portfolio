d4d519faff34e355352c150006ae2ce4bf4338f8
KathrinKath <kathrin.peled@dci-student.org>
2024-10-05 16:21:26 +0200

    updting head links for seo

---
+++ Scripts/schema.js
@@ -0,0 +1,27 @@
+// Scripts/schema.js
+const schemaData = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Kathrin Peled",
+  url: "https://draculady.de/",
+  image: "https://draculady.de/Pictures/Profile_Picture.webp",
+  jobTitle: "Web Developer",
+  worksFor: {
+    "@type": "Organization",
+    name: "Freelance"
+  },
+  sameAs: [
+    "https://www.linkedin.com/in/kathrin-peled-draculady/",
+    "https://x.com/DraculadyKath"
+  ],
+  description:
+    "Explore Kathrin Peled's portfolio showcasing her web development projects and skills."
+};
+
+// Create a <script> element
+const script = document.createElement("script");
+script.type = "application/ld+json"; // Specify the type as JSON-LD
+script.text = JSON.stringify(schemaData); // Convert the schema data to JSON format
+
+// Append the <script> to the <head>
+document.head.appendChild(script);
