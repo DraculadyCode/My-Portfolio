bfd88c473b3c267aec29042eda743d61f83d7749
KathrinKath <kathrin.peled@dci-student.org>
2024-10-07 10:09:44 +0200

    chnge descriptions of portfolios

--- Scripts/Cursor.js
+++ Scripts/Cursor.js
@@ -10,12 +10,24 @@ document.addEventListener("mousemove", (e) => {
 // Increase cursor size on click
 document.addEventListener("click", () => {
   // Increase the cursor size
-  cursor.style.width = "6rem"; // Change size to 6rem
-  cursor.style.height = "6rem"; // Change height to 6rem
+  cursor.style.width = "6rem"; // Change size to 8rem
+  cursor.style.height = "6rem"; // Change height to 8rem
 
   // Reset size back to original after a short delay
   setTimeout(() => {
-    cursor.style.width = "4rem"; // Change size back to 4rem
-    cursor.style.height = "4rem"; // Change height back to 4rem
+    cursor.style.width = "3rem"; // Change size back to 4rem
+    cursor.style.height = "3rem"; // Change height back to 4rem
   }, 5000); // Adjust the duration as needed (200 milliseconds)
+});
+// Change z-index when hovering over links
+document.querySelectorAll("a, img, i").forEach((link) => {
+  // When the mouse enters the link
+  link.addEventListener("mouseenter", () => {
+    cursor.style.zIndex = "-1"; // Set z-index to -1
+  });
+
+  // When the mouse leaves the link
+  link.addEventListener("mouseleave", () => {
+    cursor.style.zIndex = "100"; // Reset z-index back to normal
+  });
 });
