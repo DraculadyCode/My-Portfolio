90ac94fe082c2d0fdf5c9fc0b4cf025a6cd27fbb
KathrinKath <kathrin.peled@dci-student.org>
2024-10-12 15:04:40 +0200

    correction of dropdown menu for touch devices

--- Scripts/dropdown-menu.js
+++ Scripts/dropdown-menu.js
@@ -1,30 +1,6 @@
 document.querySelectorAll(".dropdown > a").forEach(function (link) {
   let timeout;
 
-  // Handle hover for non-touch devices
-  link.addEventListener("mouseover", function (e) {
-    e.preventDefault();
-    const dropdown = link.nextElementSibling;
-    if (dropdown) {
-      dropdown.style.display = "block"; // Set to block to push items down
-      dropdown.style.opacity = "1"; // Set opacity to show dropdown
-      dropdown.style.visibility = "visible"; // Ensure dropdown is visible
-      dropdown.style.position = "relative"; // Ensure dropdown is visible
-    }
-    clearTimeout(timeout);
-  });
-
-  link.addEventListener("mouseout", function () {
-    const dropdown = link.nextElementSibling;
-    if (dropdown) {
-      timeout = setTimeout(function () {
-        dropdown.style.display = "none"; // Hide dropdown after 3 seconds
-        dropdown.style.opacity = "0"; // Set opacity to hide dropdown
-        dropdown.style.visibility = "hidden"; // Hide dropdown
-      }, 3000); // Dropdown will disappear after 3 seconds
-    }
-  });
-
   // Handle touch for mobile devices
   let tappedOnce = false;
   link.addEventListener("touchstart", function (e) {
@@ -34,13 +10,12 @@ document.querySelectorAll(".dropdown > a").forEach(function (link) {
       // If already tapped once, follow the link
       return true;
     }
-
     if (dropdown && !tappedOnce) {
       e.preventDefault(); // Prevent following the link on first tap
       dropdown.style.display = "block"; // Set to block to push items down
       dropdown.style.opacity = "1"; // Set opacity to show dropdown
       dropdown.style.visibility = "visible"; // Ensure dropdown is visible
-
+      dropdown.style.position = "relative"; // Set position to relatives
       tappedOnce = true;
 
       setTimeout(function () {
