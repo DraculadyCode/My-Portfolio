09e164df9a84c43226f52d6e35b85ebb79a63faa
KathrinKath <kathrin.peled@dci-student.org>
2024-10-24 11:19:28 +0200

    updated contact pages

--- Scripts/Cursor.js
+++ Scripts/Cursor.js
@@ -1,1 +1,5 @@
-let cursor=document.querySelector(".custom-cursor");document.addEventListener("mousemove",e=>{cursor.style.left=e.pageX+"px",cursor.style.top=e.pageY+"px"}),document.addEventListener("click",()=>{cursor.style.width="3rem",cursor.style.height="3rem",setTimeout(()=>{cursor.style.width="1.5rem",cursor.style.height="1.5rem"},5e3)}),document.querySelectorAll("a, img, i").forEach(e=>{e.addEventListener("mouseenter",()=>{cursor.style.zIndex="-1"}),e.addEventListener("mouseleave",()=>{cursor.style.zIndex="100"})});
+let cursor=document.querySelector(".custom-cursor");
+document.addEventListener("mousemove",e=>{cursor.style.left=e.pageX+"px",cursor.style.top=e.pageY+"px"}),
+document.addEventListener("click",()=>{cursor.style.width="3rem",cursor.style.height="3rem",
+    setTimeout(()=>
+    {cursor.style.width="1.5rem",cursor.style.height="1.5rem"},5e3)}),document.querySelectorAll("a, img, i").forEach(e=>{e.addEventListener("mouseenter",()=>{cursor.style.zIndex="-1"}),e.addEventListener("mouseleave",()=>{cursor.style.zIndex="100"})});
