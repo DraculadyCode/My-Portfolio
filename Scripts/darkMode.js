081c2b9c4ffc6145e3f9dc9f76e10860034ba1c3
KathrinKath <kathrin.peled@dci-student.org>
2024-11-06 20:33:44 +0100

    updated resumes

--- Scripts/darkMode.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-document
-  .getElementById("toggleDarkMode")
-  .addEventListener("click", function () {
-    document.body.classList.toggle("dark-theme");
-    console.log("Dark mode toggled"); // Check if this logs to the console when clicked
-  });
