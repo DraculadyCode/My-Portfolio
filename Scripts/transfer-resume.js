933e6f4ec44a6e333651991850caa7cb6c07557c
KathrinKath <kathrin.peled@dci-student.org>
2024-11-11 09:23:08 +0100

    mobile resumetransfter JS file corrections

--- Scripts/transfer-resume.js
+++ Scripts/transfer-resume.js
@@ -3,7 +3,7 @@ function handleResolutionRedirect() {
   const currentUrl = window.location.href;
 
   if (window.innerWidth <= 1150) {
-    // Redirect to the mobile versions if on a smaller screen
+    // Redirect to mobile versions on a smaller screen
     if (currentUrl.includes("resume-english.html")) {
       window.location.href = "../Pages/resumeMobile-english.html";
     } else if (currentUrl.includes("resume-german.html")) {
@@ -13,18 +13,23 @@ function handleResolutionRedirect() {
     } else if (currentUrl.includes("timeline-german.html")) {
       window.location.href = "../Pages/resumeMobile-german.html";
     }
-    // } else if (window.innerWidth > 1150) {
-    //   // Redirect back to the desktop versions if on a larger screen
-    //   if (currentUrl.includes("resumeMobile-english.html")) {
-    //     window.location.href = "../Pages/timeline-english.html";
-    //   } else if (currentUrl.includes("resumeMobile-german.html")) {
-    //     window.location.href = "../Pages/timeline-german.html";
-    //   }
-    // }
+  } else if (window.innerWidth > 1150) {
+    // Redirect back to desktop versions on a larger screen
+    if (currentUrl.includes("resumeMobile-english.html")) {
+      window.location.href = "../Pages/resume-english.html";
+    } else if (currentUrl.includes("resumeMobile-german.html")) {
+      window.location.href = "../Pages/resume-german.html";
+    } else if (currentUrl.includes("resumeMobile-english.html")) {
+      window.location.href = "../Pages/timeline-english.html";
+    } else if (currentUrl.includes("resumeMobile-german.html")) {
+      window.location.href = "../Pages/timeline-german.html";
+    }
   }
 }
+
 // Run the redirect check when the page loads
 handleResolutionRedirect();
 
 // Attach the function to the resize event
 window.addEventListener("resize", handleResolutionRedirect);
+
