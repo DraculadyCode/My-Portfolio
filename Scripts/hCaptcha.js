5644ae09b8b23cba7db0d08a4632a0f1a4d7891d
KathrinKath <kathrin.peled@dci-student.org>
2024-11-14 15:55:54 +0100

    updated boxicon file

--- Scripts/hCaptcha.js
+++ Scripts/hCaptcha.js
@@ -1,33 +1,3 @@
-// function onCaptchaSuccess() {
-//   // Enable all links and buttons after successful CAPTCHA
-//   const linksAndButtons = document.querySelectorAll("a, button");
-//   linksAndButtons.forEach((element) => {
-//     element.classList.remove("disabled");
-//     element.style.cursor = "pointer"; // Reset cursor to default
-//   });
-// }
-
-// // Disable links and buttons by default, except for the language switcher
-// document.addEventListener("DOMContentLoaded", function () {
-//   const linksAndButtons = document.querySelectorAll("a, button");
-//   linksAndButtons.forEach((element) => {
-//     // Skip disabling if the link is part of the language switcher
-//     if (element.closest("#language-switcher")) return;
-
-//     // Disable other links and buttons
-//     element.classList.add("disabled");
-//     element.style.cursor = "not-allowed"; // Change cursor to 'not-allowed'
-
-//     element.addEventListener("click", (e) => {
-//       if (element.classList.contains("disabled")) {
-//         e.preventDefault(); // Prevent default action
-//         alert("Please verify you are a human first!"); // Show alert message
-//         window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top
-//       }
-//     });
-//   });
-// });
-
 function onCaptchaSuccess() {
   // Enable all links and buttons after successful CAPTCHA
   const links = document.querySelectorAll("a, button");
